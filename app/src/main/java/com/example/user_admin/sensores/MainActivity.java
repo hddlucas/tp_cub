36212f98b86352ccc236f3b0720adb0a195c91bf
Hugo Lucas <hddlucas94@gmail.com>
2018-03-24 18:18:56 +0000

    added checkbox for automatic mode (not used for now)

--- app/src/main/java/com/example/user_admin/sensores/MainActivity.java
+++ app/src/main/java/com/example/user_admin/sensores/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user_admin.sensores;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -15,10 +16,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
-    // GPSTracker class
+
+    //Classes
     GPSTracker gps;
+    FileManager fm;
+    Permissions permissions;
+
+    //variables
     public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    private  String TAG_WRITE_READ_FILE = "TAG_WRITE_READ_FILE";
+    private String dataFileName = "sensores.csv";
 
     //elements
     Button startBtn;
@@ -29,25 +40,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-        } else {
-            // No explanation needed, we can request the permission.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-        }
-
         startBtn = (Button) findViewById(R.id.startBtn);
         logsTxtBox = (TextView) findViewById(R.id.logsTxtBox);
+
+        //check location permissions (run time permissions)
+        permissions = new Permissions(MainActivity.this);
+        permissions.checkLocationPermissions();
+
+        //check external write permissions
+        permissions.checkWriteExternalStoragePermission();
 
         // show location button click event
         startBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
+
                 // create class object
                 gps = new GPSTracker(MainActivity.this);
 
@@ -56,11 +64,23 @@ public class MainActivity extends AppCompatActivity {
 
                     double latitude = gps.getLatitude();
                     double longitude = gps.getLongitude();
+                    double altitude = gps.getAltitude();
 
                     //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
 
                     TextView logsTxtBox = (TextView) findViewById(R.id.logsTxtBox);
-                    logsTxtBox.append("Posição Atual: Lat: " + latitude + " Long: " + longitude + "\n");
+                    logsTxtBox.append("Posição Atual: Lat: " + latitude + " Long: " + longitude + " Altitude: " + altitude + "\n");
+                    String filename = "myfile";
+                    String fileContents = "Hello world!";
+                    FileOutputStream outputStream;
+
+                    try {
+                        outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                        outputStream.write(fileContents.getBytes());
+                        outputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
                 } else {
                     // can't get location
@@ -72,8 +92,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void stopBtnClick(View view) {
+
+
+    public void stopBtnClick(View view) throws FileNotFoundException {
+
+        //stop using gps
         gps.stopUsingGPS();
+        logsTxtBox.setText("");
         Toast.makeText(this, "Pausou a recolha de dados!", Toast.LENGTH_SHORT).show();
     }
 
